ee0cbb7e39083264282b1ca52ffaff4bc0849ead
siris <sirisgadson@gmail.com>
2016-12-21 00:38:09 -0500

    first checkin

---
+++ app/src/main/java/notifications/mac/com/mynotifications/MainActivity.java
@@ -0,0 +1,38 @@
+package notifications.mac.com.mynotifications;
+
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.app.TaskStackBuilder;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v4.app.NotificationCompat;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        NotificationCompat.Builder mBuilder =
+                new NotificationCompat.Builder(this)
+                        .setSmallIcon(R.mipmap.cloud_clip)
+                        .setContentTitle("My notification")
+                        .setContentText("Hello World!");
+
+        // Sets an ID for the notification
+        int mNotificationId = 001;
+// Gets an instance of the NotificationManager service
+        NotificationManager mNotifyMgr =
+                (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+// Builds the notification and issues it.
+        mNotifyMgr.notify(mNotificationId, mBuilder.build());
+    }
+}
